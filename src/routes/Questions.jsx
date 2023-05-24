7f9b0e0dca71bfb21e5fd4c9664d289f9b6b0f4c
Ioannis Potouridis <potouridisio@gmail.com>
2023-05-24 11:37:23 +0300

    feat: add the question card layout

--- src/routes/Questions.jsx
+++ src/routes/Questions.jsx
@@ -6,6 +6,49 @@ export default function Questions() {
       <h2 className="mb-[0.35em] text-2xl leading-[1.334] tracking-normal">
         All Questions
       </h2>
+
+      <div className="w-full">
+        <div className="flex flex-col space-y-4">
+          {/* Question */}
+          <div className="overflow-hidden rounded bg-white text-black text-opacity-[0.87] shadow-[rgba(0,_0,_0,_0.2)_0px_2px_1px_-1px,_rgba(0,_0,_0,_0.14)_0px_1px_1px_0px,_rgba(0,_0,_0,_0.12)_0px_1px_3px_0px]">
+            <div className="p-4">
+              <p className="mb-[0.35em] text-sm leading-normal tracking-[0.00938em] text-black text-opacity-60">
+                4 votes 1 answer
+              </p>
+              <div className="m-0 text-2xl leading-[1.334] tracking-normal">
+                How to loop through an array in JavaScript?
+              </div>
+              <p className="mb-3 leading-normal tracking-[0.00938em] text-black text-opacity-60">
+                I have an array of items and need to iterate over them. What is
+                the best way to achieve this?
+              </p>
+              <p className="m-0 text-sm leading-[1.43] tracking-[0.01071em]">
+                John Doe 1000 asked 1 year ago
+              </p>
+            </div>
+
+            <div className="flex items-center p-2">
+              <div className="flex flex-row space-x-2">
+                <div
+                  className="relative m-0 box-border inline-flex h-8 max-w-full cursor-pointer select-none appearance-none items-center justify-center whitespace-nowrap rounded-2xl border-0 bg-black bg-opacity-[0.08] p-0 align-middle text-[0.8125rem] text-black text-opacity-[0.87] no-underline outline-0 hover:bg-opacity-[0.12]"
+                  role="button"
+                  tabIndex={0}
+                >
+                  <span className="truncate px-3">javascript</span>
+                </div>
+
+                <div
+                  className="relative m-0 box-border inline-flex h-8 max-w-full cursor-pointer select-none appearance-none items-center justify-center whitespace-nowrap rounded-2xl border-0 bg-black bg-opacity-[0.08] p-0 align-middle text-[0.8125rem] text-black text-opacity-[0.87] no-underline outline-0 hover:bg-opacity-[0.12]"
+                  role="button"
+                  tabIndex={0}
+                >
+                  <span className="truncate px-3">css</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
