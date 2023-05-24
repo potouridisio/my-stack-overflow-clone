aa4a3abf424a437b8835a22c67a7f8b04a0e8d0a
Ioannis Potouridis <potouridisio@gmail.com>
2023-05-24 09:28:35 +0300

    feat: add the primary search field

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { GlobeAmericasIcon } from "@heroicons/react/24/solid";
 
 export default function App() {
@@ -8,6 +9,19 @@ export default function App() {
         <div className="relative flex min-h-[4rem] items-center px-6">
           <div className="m-0 truncate text-xl font-medium leading-[1.6] tracking-[0.0075em]">
             Stack Overflow Clone
+          </div>
+
+          {/* Search */}
+          <div className="relative ml-6 mr-4 w-auto rounded bg-white bg-opacity-[0.15] hover:bg-opacity-[0.25]">
+            <div className="pointer-events-none absolute flex h-full items-center justify-center px-4">
+              <MagnifyingGlassIcon className="inline-block h-[1em] w-[1em] shrink-0 select-none stroke-current text-2xl" />
+            </div>
+            <div className="relative box-border inline-flex cursor-text items-center leading-[1.4375em] tracking-[0.00938em] text-inherit">
+              <input
+                className="m-0 box-content block h-[1.4375em] w-full min-w-0 border-0 p-2 pl-[calc(1em_+_32px)] tracking-[inherit] text-current [background:none] [font:inherit] placeholder:!block placeholder:text-current placeholder:opacity-[0.42] focus:outline-0"
+                placeholder="Search…"
+              />
+            </div>
           </div>
         </div>
       </header>
