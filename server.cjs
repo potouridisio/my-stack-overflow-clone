dd2348bd279e5f263917b5839aec5ad0eaf42acc
Ioannis Potouridis <potouridisio@gmail.com>
2023-05-26 05:54:00 +0300

    feat: add the endpoint to get answers of a specific question

--- server.cjs
+++ server.cjs
@@ -255,6 +255,119 @@ app.get("/questions/:id", (req, res) => {
   }
 });
 
+const answers = [
+  {
+    id: 1,
+    questionId: 1,
+    body: "One of the recommended ways to loop through an array in JavaScript is to use the 'forEach' method. It provides a more concise syntax and handles the iteration automatically. Here's an example:",
+    userId: 2,
+    createdAt: "2023-05-21T12:15:00Z",
+    updatedAt: "2023-05-22T09:45:00Z",
+  },
+  {
+    id: 2,
+    questionId: 2,
+    body: "When it comes to responsive web design, using CSS media queries is a common approach. You can define different CSS rules based on the screen size or device type. Here's an example of a media query:",
+    userId: 3,
+    createdAt: "2023-05-22T10:30:00Z",
+    updatedAt: "2023-05-23T08:20:00Z",
+  },
+  {
+    id: 3,
+    questionId: 2,
+    body: "In addition to CSS media queries, you can leverage CSS frameworks like Bootstrap or Foundation that provide responsive design components and grid systems. These frameworks offer pre-defined CSS classes that you can use to build responsive layouts more easily.",
+    userId: 1,
+    createdAt: "2023-05-22T15:45:00Z",
+    updatedAt: "2023-05-23T09:50:00Z",
+  },
+  {
+    id: 4,
+    questionId: 2,
+    body: "Another tip for responsive web design is to use relative units like percentages or 'em' instead of fixed pixel values for sizing elements. This allows the content to adapt to different screen sizes more effectively.",
+    userId: 2,
+    createdAt: "2023-05-23T11:20:00Z",
+    updatedAt: "2023-05-24T07:55:00Z",
+  },
+  {
+    id: 5,
+    questionId: 3,
+    body: "Both JWT and OAuth are widely used authentication methods. JWT (JSON Web Tokens) is a compact and self-contained token format that can securely transmit information between parties. OAuth is an open standard for authorization that allows users to grant third-party applications limited access to their resources.",
+    userId: 3,
+    createdAt: "2023-05-23T16:10:00Z",
+    updatedAt: "2023-05-24T09:30:00Z",
+  },
+  {
+    id: 6,
+    questionId: 3,
+    body: "The choice between JWT and OAuth depends on the specific requirements of your project. JWT is simpler to implement and is suitable for scenarios where you have full control over the authentication process. OAuth, on the other hand, is more complex but provides additional features like delegated authorization and centralized user management.",
+    userId: 1,
+    createdAt: "2023-05-23T18:25:00Z",
+    updatedAt: "2023-05-24T11:45:00Z",
+  },
+  {
+    id: 7,
+    questionId: 4,
+    body: "When it comes to database normalization, it's essential to follow a set of best practices. Here are a few guidelines:",
+    userId: 2,
+    createdAt: "2023-05-24T10:05:00Z",
+    updatedAt: "2023-05-24T15:20:00Z",
+  },
+  {
+    id: 8,
+    questionId: 4,
+    body: "1. Identify the entities and their relationships: Start by identifying the entities in your system and how they relate to each other.",
+    userId: 1,
+    createdAt: "2023-05-24T12:30:00Z",
+    updatedAt: "2023-05-25T08:40:00Z",
+  },
+  {
+    id: 9,
+    questionId: 4,
+    body: "2. Eliminate data redundancy: Ensure that each piece of data is stored only once to avoid data inconsistencies and save storage space.",
+    userId: 3,
+    createdAt: "2023-05-24T14:15:00Z",
+    updatedAt: "2023-05-25T09:20:00Z",
+  },
+  {
+    id: 10,
+    questionId: 4,
+    body: "3. Use primary keys and foreign keys: Establish relationships between tables using primary keys and foreign keys to maintain data integrity.",
+    userId: 2,
+    createdAt: "2023-05-24T16:40:00Z",
+    updatedAt: "2023-05-25T10:05:00Z",
+  },
+  {
+    id: 11,
+    questionId: 5,
+    body: "JavaScript Promises provide a way to handle asynchronous operations. They represent a value that may not be available yet, but will be resolved in the future. Here's an example of using a Promise:",
+    userId: 2,
+    createdAt: "2023-05-26T12:30:00Z",
+    updatedAt: "2023-05-26T15:45:00Z",
+  },
+  {
+    id: 12,
+    questionId: 5,
+    body: "Async/await is a modern JavaScript feature that simplifies asynchronous code. It allows you to write asynchronous code that looks and behaves more like synchronous code, making it easier to understand and maintain. Here's an example:",
+    userId: 1,
+    createdAt: "2023-05-26T16:00:00Z",
+    updatedAt: "2023-05-26T18:20:00Z",
+  },
+];
+
+app.get("/questions/:id/answers", (req, res) => {
+  const { id } = req.params;
+  const question = questions.find((q) => q.id === parseInt(id));
+
+  if (question) {
+    const questionAnswers = answers.filter(
+      (answer) => answer.questionId === question.id
+    );
+    res.json(questionAnswers);
+  } else {
+    res.status(404).json({ error: "Question not found" });
+  }
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
