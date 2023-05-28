c9aa67cf76f50fb20f6401c16b80c9c8d3451e94
Konstantinos Pavlos <kpav1991@gmail.com>
2023-05-28 20:15:08 +0300

    Adding style in selected tag

--- src/routes/Tags.jsx
+++ src/routes/Tags.jsx
@@ -25,6 +25,7 @@ export function loader({ request }) {
 export default function Tags() {
   const tags = useLoaderData();
   const [params, setParams] = useSearchParams();
+  const tab = params.get("tab");
 
   return (
     <main className="grow p-6">
@@ -41,15 +42,19 @@ export default function Tags() {
         <div className="relative flex min-h-[4rem] items-center justify-end">
           <div className="inline-flex rounded" role="group">
             <button
-              className={`relative m-0 box-border inline-flex cursor-pointer select-none appearance-none items-center justify-center rounded rounded-br-none rounded-tr-none border border-solid border-black border-opacity-[0.12]${
+              className={`relative m-0 box-border inline-flex cursor-pointer select-none appearance-none items-center 
+                          justify-center rounded rounded-br-none rounded-tr-none border border-solid border-black 
+                          border-opacity-[0.12]${
+                            // eslint-disable-next-line no-constant-condition
+                            tab === "popular"
+                              ? " bg-[rgba(25,_118,_210,_0.08)]"
+                              : ""
+                          } p-[7px] align-middle text-[0.8125rem] font-medium uppercase leading-[1.75] tracking-[0.02857em]${
                 // eslint-disable-next-line no-constant-condition
-                true ? " bg-[rgba(25,_118,_210,_0.08)]" : ""
-              } p-[7px] align-middle text-[0.8125rem] font-medium uppercase leading-[1.75] tracking-[0.02857em]${
-                // eslint-disable-next-line no-constant-condition
-                true ? " text-[rgb(25,_118,_210)]" : ""
+                tab === "popular" ? " text-[rgb(25,_118,_210)]" : ""
               } no-underline outline-0 ${
                 // eslint-disable-next-line no-constant-condition
-                true
+                tab === "popular"
                   ? "hover:bg-[rgba(25,_118,_210,_0.12)]"
                   : "hover:bg-[rgba(0,_0,_0,_0.04)]"
               } hover:no-underline`}
@@ -60,7 +65,22 @@ export default function Tags() {
             </button>
 
             <button
-              className="relative m-0 -ml-px box-border inline-flex cursor-pointer select-none appearance-none items-center justify-center rounded rounded-bl-none rounded-br-none rounded-tl-none rounded-tr-none border border-solid border-black border-l-transparent border-opacity-[0.12] p-[7px] align-middle text-[0.8125rem] font-medium uppercase leading-[1.75] tracking-[0.02857em] no-underline outline-0 hover:bg-[rgba(0,_0,_0,_0.04)] hover:no-underline"
+              className={`relative m-0 box-border inline-flex cursor-pointer select-none appearance-none items-center 
+                          justify-center rounded rounded-br-none rounded-tr-none border border-solid border-black 
+                          border-opacity-[0.12]${
+                            // eslint-disable-next-line no-constant-condition
+                            tab === "name"
+                              ? " bg-[rgba(25,_118,_210,_0.08)]"
+                              : ""
+                          } p-[7px] align-middle text-[0.8125rem] font-medium uppercase leading-[1.75] tracking-[0.02857em]${
+                // eslint-disable-next-line no-constant-condition
+                tab === "name" ? " text-[rgb(25,_118,_210)]" : ""
+              } no-underline outline-0 ${
+                // eslint-disable-next-line no-constant-condition
+                tab === "name"
+                  ? "hover:bg-[rgba(25,_118,_210,_0.12)]"
+                  : "hover:bg-[rgba(0,_0,_0,_0.04)]"
+              } hover:no-underline`}
               type="button"
               onClick={() => setParams({ tab: "name" })}
             >
@@ -68,7 +88,20 @@ export default function Tags() {
             </button>
 
             <button
-              className="relative m-0 -ml-px box-border inline-flex cursor-pointer select-none appearance-none items-center justify-center rounded rounded-bl-none rounded-tl-none border border-solid border-black border-l-transparent border-opacity-[0.12] p-[7px] align-middle text-[0.8125rem] font-medium uppercase leading-[1.75] tracking-[0.02857em] no-underline outline-0 hover:bg-[rgba(0,_0,_0,_0.04)] hover:no-underline"
+              className={`relative m-0 box-border inline-flex cursor-pointer select-none appearance-none items-center 
+                        justify-center rounded rounded-br-none rounded-tr-none border border-solid border-black 
+                        border-opacity-[0.12]${
+                          // eslint-disable-next-line no-constant-condition
+                          tab === "new" ? " bg-[rgba(25,_118,_210,_0.08)]" : ""
+                        } p-[7px] align-middle text-[0.8125rem] font-medium uppercase leading-[1.75] tracking-[0.02857em]${
+                // eslint-disable-next-line no-constant-condition
+                tab === "new" ? " text-[rgb(25,_118,_210)]" : ""
+              } no-underline outline-0 ${
+                // eslint-disable-next-line no-constant-condition
+                tab === "new"
+                  ? "hover:bg-[rgba(25,_118,_210,_0.12)]"
+                  : "hover:bg-[rgba(0,_0,_0,_0.04)]"
+              } hover:no-underline`}
               type="button"
               onClick={() => setParams({ tab: "new" })}
             >
