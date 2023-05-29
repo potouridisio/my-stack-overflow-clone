7601688b6ddc7e91c6ae17fef005aa84f852af13
Ioannis Potouridis <potouridisio@gmail.com>
2023-05-29 19:06:48 +0300

    feat: add validation to new question form

--- src/routes/Ask.jsx
+++ src/routes/Ask.jsx
@@ -1,4 +1,4 @@
-import { Form, redirect } from "react-router-dom";
+import { Form, redirect, useActionData } from "react-router-dom";
 
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
@@ -11,6 +11,26 @@ import TextField from "@mui/material/TextField";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 
+function validateQuestionBody(body) {
+  if (!body) {
+    return "Body is missing.";
+  }
+
+  if (body.length < 30) {
+    return `Body must be at least 30 characters; you entered ${body.length}.`;
+  }
+}
+
+function validateQuestionTitle(title) {
+  if (!title) {
+    return "Title is missing.";
+  }
+
+  if (title.length < 15) {
+    return "Title must be at least 15 characters.";
+  }
+}
+
 // eslint-disable-next-line react-refresh/only-export-components
 export async function action({ request }) {
   const formData = await request.formData();
@@ -21,6 +41,17 @@ export async function action({ request }) {
     tagIds: [],
     userId: 1,
   };
+
+  const fieldErrors = {
+    body: validateQuestionBody(newQuestion.body),
+    title: validateQuestionTitle(newQuestion.title),
+  };
+
+  if (Object.values(fieldErrors).some(Boolean)) {
+    return {
+      fieldErrors,
+    };
+  }
 
   const response = await fetch("/api/questions", {
     method: "POST",
@@ -36,6 +67,8 @@ export async function action({ request }) {
 }
 
 export default function Ask() {
+  const actionData = useActionData();
+
   return (
     <Box sx={{ flexGrow: 1, p: 3 }}>
       <Toolbar />
@@ -53,14 +86,23 @@ export default function Ask() {
           <Stack component={Form} id="new-question" method="post" spacing={2}>
             <TextField
               autoFocus
-              helperText="Be specific and imagine you're asking a question to another person"
+              // optional chaining operator
+              error={actionData?.fieldErrors?.title}
+              helperText={
+                actionData?.fieldErrors?.title ||
+                "Be specific and imagine you're asking a question to another person"
+              }
               label="Title"
               name="title"
               placeholder="e.g. Is there an R function for finding the index of an element in a vector?"
               size="small"
             />
             <TextField
-              helperText="Include all the information someone would need to answer your question"
+              error={actionData?.fieldErrors?.body}
+              helperText={
+                actionData?.fieldErrors?.body ||
+                "Include all the information someone would need to answer your question"
+              }
               label="Body"
               multiline
               name="body"
