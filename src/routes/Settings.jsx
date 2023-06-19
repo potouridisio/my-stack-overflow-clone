9dae01c37438e975e0fb49d354601a337c63eca9
Konstantinos Pavlos <kpav1991@gmail.com>
2023-06-19 16:15:33 +0300

    feat: Create User Page with the Settings option

---
+++ src/routes/Settings.jsx
@@ -0,0 +1,165 @@
+import { Link, Outlet, useOutletContext } from "react-router-dom";
+
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import ListSubHeader from "@mui/material/ListSubheader";
+
+const drawerWidth = 240;
+
+export default function Settings() {
+  return (
+    <Box sx={{ display: "flex" }}>
+      <Drawer
+        PaperProps={{ sx: { position: "relative" } }}
+        sx={{
+          [`& .MuiDrawer-paper`]: {
+            boxSizing: "border-box",
+            width: drawerWidth,
+          },
+          flexShrink: 0,
+          width: drawerWidth,
+        }}
+        variant="permanent"
+      >
+        <Box
+          sx={{
+            overflow: "auto",
+            display: "flex",
+            flexDirection: "column",
+            gap: 1,
+          }}
+        >
+          <List
+            subheader={
+              <ListSubHeader sx={{ fontWeight: 700, pt: 1 }}>
+                PERSONAL INFORMATION
+              </ListSubHeader>
+            }
+          >
+            <ListItem disablePadding>
+              <ListItemButton>
+                <Link style={{ textDecoration: "none" }}>
+                  {" "}
+                  <ListItemText primary="Edit profile" />
+                </Link>
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary="Delete profile" />
+              </ListItemButton>
+            </ListItem>
+          </List>
+
+          <List
+            subheader={
+              <ListSubHeader sx={{ fontWeight: 700 }}>
+                EMAIL SETTINGS
+              </ListSubHeader>
+            }
+          >
+            <ListItem disablePadding>
+              <ListItemButton>
+                <Link to={"email/settings"} style={{ textDecoration: "none" }}>
+                  <ListItemText primary="Edit email settings" />
+                </Link>
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <Link
+                  to={"tag-notifications"}
+                  style={{ textDecoration: "none" }}
+                >
+                  <ListItemText primary="Tag watching & ignoring" />
+                </Link>
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary="Community digests" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary="Question subscriptions" />
+              </ListItemButton>
+            </ListItem>
+          </List>
+
+          <List
+            subheader={
+              <ListSubHeader sx={{ fontWeight: 700 }}>
+                SITE SETTINGS
+              </ListSubHeader>
+            }
+          >
+            <ListItem disablePadding>
+              <ListItemButton>
+                <Link to={"preferences"} style={{ textDecoration: "none" }}>
+                  {" "}
+                  <ListItemText primary="Preferences" />
+                </Link>
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <Link to={"JohnDoe/flair"} style={{ textDecoration: "none" }}>
+                  <ListItemText primary="Flair" />
+                </Link>
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <Link to={"hidecommunities"} style={{ textDecoration: "none" }}>
+                  <ListItemText primary="Hide Communities" />
+                </Link>
+              </ListItemButton>
+            </ListItem>
+          </List>
+
+          <List
+            subheader={
+              <ListSubHeader sx={{ fontWeight: 700 }}>ACCESS</ListSubHeader>
+            }
+          >
+            <ListItem disablePadding>
+              <ListItemButton>
+                <Link style={{ textDecoration: "none" }}>
+                  {" "}
+                  <ListItemText primary="Your collectives" />
+                </Link>
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary="Your logins" />
+              </ListItemButton>
+            </ListItem>
+          </List>
+
+          <List
+            subheader={
+              <ListSubHeader sx={{ fontWeight: 700 }}>API</ListSubHeader>
+            }
+          >
+            <ListItem disablePadding>
+              <ListItemButton>
+                <Link style={{ textDecoration: "none" }}>
+                  {" "}
+                  <ListItemText primary="Authorized applications" />
+                </Link>
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </Box>
+      </Drawer>
+
+      <Outlet context={useOutletContext()} />
+    </Box>
+  );
+}
---
+++ src/routes/User.jsx
@@ -0,0 +1,116 @@
+import { Link, Outlet, useOutletContext } from "react-router-dom";
+
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+
+import Toolbar from "@mui/material/Toolbar";
+import PersonIcon from "@mui/icons-material/Person";
+
+import Typography from "@mui/material/Typography";
+
+import CakeIcon from "@mui/icons-material/Cake";
+import WatchLaterIcon from "@mui/icons-material/WatchLater";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import EditIcon from "@mui/icons-material/Edit";
+import InsertCommentIcon from "@mui/icons-material/InsertComment";
+
+const tabClass = {
+  borderRadius: "40%",
+  mr: 2,
+  padding: 1,
+  textTransform: "none",
+};
+
+export default function User() {
+  return (
+    <>
+      <Box component="main" sx={{ flexGrow: 1, mt: 2, ml: 2 }}>
+        <Toolbar />
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <Box sx={{ display: "flex" }}>
+            <PersonIcon sx={{ fontSize: 140 }} />
+
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+              }}
+            >
+              <Typography sx={{ fontSize: 23 }}>John Doe</Typography>
+              <Box sx={{ display: "flex", gap: 2 }}>
+                <CakeIcon sx={{ fontSize: 20 }} />
+                <Typography sx={{ fontSize: 14, mr: 2 }}>
+                  Member for 1 year
+                </Typography>
+                <WatchLaterIcon sx={{ fontSize: 20 }} />
+                <Typography sx={{ fontSize: 14, mr: 2 }}>
+                  Last seen this week
+                </Typography>
+                <Button
+                  sx={{
+                    p: 0,
+                    color: "black",
+                    fontWeight: 400,
+                    textTransform: "none",
+                  }}
+                  title="Click to view daily visit calendar"
+                >
+                  <CalendarMonthIcon
+                    sx={{ fontSize: 20, color: "black", mr: 2 }}
+                  />
+                  Visited 23 days, 20 consecutive
+                </Button>
+              </Box>
+            </Box>
+
+            <Box
+              sx={{
+                display: "flex",
+                gap: 2,
+                flexGrow: 1,
+                mr: 2,
+                alignItems: "flex-start",
+                justifyContent: "flex-end",
+              }}
+            >
+              <Button sx={{ border: 1, color: "black" }}>
+                <EditIcon sx={{ fontSize: 20 }} />
+                <Link to={""} style={{ textDecoration: "none" }}>
+                  {" "}
+                  Edit profile
+                </Link>
+              </Button>
+              <Button sx={{ border: 1, color: "black" }}>
+                <InsertCommentIcon sx={{ fontSize: 20, color: "blue" }} />
+                <Link to={""} style={{ textDecoration: "none" }}>
+                  {" "}
+                  Network profile
+                </Link>
+              </Button>
+            </Box>
+          </Box>
+
+          <Tabs>
+            <Link to={"users/1/John-Doe"}>
+              <Tab label="Profile" value="profile" sx={tabClass} />
+            </Link>
+            <Link to={"users/1/John-Doe"}>
+              <Tab label="Activity" value="activity" sx={tabClass} />
+            </Link>
+            <Link to={"users/1/saves/all"}>
+              <Tab label="Saves" value="saves" sx={tabClass} />
+            </Link>
+            <Link to={"users/1"}>
+              <Tab label="Settings" value="settings" sx={tabClass} />
+            </Link>
+          </Tabs>
+        </Box>
+
+        <Outlet context={useOutletContext()} />
+      </Box>
+    </>
+  );
+}
