60f2808c7bd083991ebb8523d071ecc7b9687edf
Ioannis Potouridis <potouridisio@gmail.com>
2023-06-21 18:53:49 +0300

    fix: fixes User tabs issue

--- src/routes/User.jsx
+++ src/routes/User.jsx
@@ -1,53 +1,31 @@
-import {
-  Link,
-  matchPath,
-  Outlet,
-  useLocation,
-  useOutletContext,
-} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, Outlet, useLocation, useOutletContext } from "react-router-dom";
 
 import Box from "@mui/material/Box";
 import Tab from "@mui/material/Tab";
 import Tabs from "@mui/material/Tabs";
 import Toolbar from "@mui/material/Toolbar";
 
-function useRouteMatch(patterns) {
-  const { pathname } = useLocation();
-
-  for (let i = 0; i < patterns.length; i += 1) {
-    const pattern = patterns[i];
-    const possibleMatch = matchPath(pattern, pathname);
-
-    if (possibleMatch !== null) {
-      return possibleMatch;
-    }
-  }
-
-  return null;
-}
-
 export default function User() {
-  const routeMatch = useRouteMatch(["/users/1/saves", "/users/1/*"]);
-  const currentTab = routeMatch?.pattern?.path;
+  const { pathname } = useLocation();
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    if (pathname.includes("saves")) {
+      setValue(0);
+    } else {
+      setValue(1);
+    }
+  }, [pathname]);
 
   return (
     <Box component="main" sx={{ flexGrow: 1 }}>
       <Toolbar />
 
       <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-        <Tabs value={currentTab}>
-          <Tab
-            component={Link}
-            label="Saves"
-            to="saves"
-            value="/users/1/saves"
-          />
-          <Tab
-            component={Link}
-            label="Settings"
-            to="preferences"
-            value="/users/1/*"
-          />
+        <Tabs value={value}>
+          <Tab component={Link} label="Saves" to="saves" />
+          <Tab component={Link} label="Settings" to="preferences" />
         </Tabs>
       </Box>
 
