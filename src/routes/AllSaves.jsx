f33d15416dc17ecd566a049157df20ea9301ea75
Ioannis Potouridis <potouridisio@gmail.com>
2023-06-23 09:31:10 +0300

    fix: remove unused CardContent import

--- src/routes/AllSaves.jsx
+++ src/routes/AllSaves.jsx
@@ -5,7 +5,6 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
 import CardHeader from "@mui/material/CardHeader";
 import Chip from "@mui/material/Chip";
 import IconButton from "@mui/material/IconButton";
