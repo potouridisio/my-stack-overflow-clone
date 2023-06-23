f894387f117311822626f82a6c6e1b46833f72c8
Dimitris iwannidis <iwannidisdimitrioss@gmail.com>
2023-06-23 17:31:36 +0300

    adding spinner on request

--- src/routes/Tags.jsx
+++ src/routes/Tags.jsx
@@ -15,9 +15,13 @@ import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Unstable_Grid2";
+import CircularProgress from "@mui/material/CircularProgress";
+import { useState } from "react";
 
 // eslint-disable-next-line react-refresh/only-export-components
-export function loader({ request }) {
+export function Tagsloader({ request, setLoading }) {
+  // const [loading, setLoading] = useState(true);
+
   const url = new URL(request.url);
   const tab = url.searchParams.get("tab");
   const page = url.searchParams.get("page") || 1;
@@ -32,16 +36,25 @@ export function loader({ request }) {
     sortBy = "latest";
   }
 
-  return fetch(`/api/tags?page=${page}${sortBy ? `&sortBy=${sortBy}` : ""}`);
+  setLoading(true);
+
+  return fetch(
+    `/api/tags?page=${page}${sortBy ? `&sortBy=${sortBy}` : ""}`
+  ).then((response) => {
+    setLoading(false);
+    return response;
+  });
 }
 
 export default function Tags() {
+  const [loading, setLoading] = useState(false);
   const tagsResponse = useLoaderData();
   const [searchParams, setSearchParams] = useSearchParams();
   const tab = searchParams.get("tab") || "popular";
 
   const handleChangeTab = (_event, value) => {
     if (value) {
+      setLoading(true);
       const newSearchParams = new URLSearchParams(searchParams);
 
       newSearchParams.set("tab", value);
@@ -51,6 +64,8 @@ export default function Tags() {
   };
 
   const handleChangePage = (_event, page) => {
+    setLoading(true);
+
     const newSearchParams = new URLSearchParams(searchParams);
 
     newSearchParams.set("page", page);
@@ -99,27 +114,44 @@ export default function Tags() {
           </ToggleButtonGroup>
         </Toolbar>
 
-        <Grid container spacing={2}>
-          {tagsResponse.tags.map((tag) => (
-            <Grid key={tag.id} xs={3}>
-              <Card>
-                <CardContent>
-                  <Chip label={tag.name} onClick={() => {}} sx={{ mb: 1.5 }} />
-                  <Typography color="text.secondary">
-                    {tag.description}
-                  </Typography>
-                </CardContent>
+        {loading ? (
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "200px",
+            }}
+          >
+            <CircularProgress />
+          </Box>
+        ) : (
+          <Grid container spacing={2}>
+            {tagsResponse.tags.map((tag) => (
+              <Grid key={tag.id} xs={3}>
+                <Card>
+                  <CardContent>
+                    <Chip
+                      label={tag.name}
+                      onClick={() => {}}
+                      sx={{ mb: 1.5 }}
+                    />
+                    <Typography color="text.secondary">
+                      {tag.description}
+                    </Typography>
+                  </CardContent>
 
-                <CardActions sx={{ justifyContent: "flex-end" }}>
-                  <Typography variant="body2">
-                    {tag.occurrenceCount} question
-                    {tag.occurrenceCount === 1 ? "" : "s"}
-                  </Typography>
-                </CardActions>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
+                  <CardActions sx={{ justifyContent: "flex-end" }}>
+                    <Typography variant="body2">
+                      {tag.occurrenceCount} question
+                      {tag.occurrenceCount === 1 ? "" : "s"}
+                    </Typography>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        )}
 
         <Toolbar sx={{ justifyContent: "flex-end" }}>
           <Pagination
